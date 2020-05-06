3adcae123491c399e50b50fffb4e1959188f85ed
Javi <javibarrientosfernandez99@gmail.com>
2020-05-06 12:04:22 +0200

    refactor, fix: Refactorizar logica, arreglar error nombre de class

--- setmana_2_practica/src/main/java/org/formacio/mvc/Controlador.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/Controlador.java
@@ -5,10 +5,11 @@ import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MockMvc {
+public class Controlador {
 	
 	// test_obte_nombre_contactes()
 	
@@ -27,8 +28,9 @@ public class MockMvc {
 	@RequestMapping(path="/telefon")
 	@ResponseBody
 	String numeroDeTelefono(String id) {
-		Persona persona = agenda.recupera(id);
-		String numeroPersona = persona.getTelefon();
-		return numeroPersona;
+		String numero = agenda.recupera(id).getTelefon();
+		return numero;
 	}
+	
+	// test_contacte()
 }
