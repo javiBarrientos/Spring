17a10b38f41c963da2f1f913d3dce2174b5d42ce
Javi <javibarrientosfernandez99@gmail.com>
2020-05-06 08:53:16 +0200

    feat: Añadir logica segundo caso test, error 404

--- setmana_2_practica/src/main/java/org/formacio/mvc/MockMvc.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/MockMvc.java
@@ -1,6 +1,7 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MockMvc {
+	
+	// test_obte_nombre_contactes()
 	
 	@Autowired
 	private AgendaService agenda;
@@ -17,5 +20,15 @@ public class MockMvc {
 	public String numeroDeContactos() {
 		int numero = agenda.nombreContactes();
 		return String.valueOf(numero);
+	}
+	
+	// test_obte_telefon()
+	
+	@RequestMapping(path="/telefon")
+	@ResponseBody
+	String numeroDeTelefono(String id) {
+		Persona persona = agenda.recupera(id);
+		String numeroPersona = persona.getTelefon();
+		return numeroPersona;
 	}
 }
