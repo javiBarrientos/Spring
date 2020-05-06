914214581e8ac8ea3a2e5cb972e3b62aeb4e4d8a
Javi <javibarrientosfernandez99@gmail.com>
2020-05-06 08:35:11 +0200

    feat: Añadir mock con primer caso test pasado

---
+++ setmana_2_practica/src/main/java/org/formacio/mvc/MockMvc.java
@@ -0,0 +1,21 @@
+package org.formacio.mvc;
+
+import org.formacio.repositori.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MockMvc {
+	
+	@Autowired
+	private AgendaService agenda;
+
+	@RequestMapping(path="/nombre")
+	@ResponseBody
+	public String numeroDeContactos() {
+		int numero = agenda.nombreContactes();
+		return String.valueOf(numero);
+	}
+}
