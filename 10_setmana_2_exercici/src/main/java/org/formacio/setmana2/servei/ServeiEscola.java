5bee76df7779b6c3245d677a304457e70b2fade5
Javi <javibarrientosfernandez99@gmail.com>
2020-05-20 21:40:17 +0200

    refactor: Eliminar imports innecesarios

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -3,15 +3,12 @@ package org.formacio.setmana2.servei;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.formacio.setmana2.domini.Matricula;
 import org.formacio.setmana2.repositori.EdatIncorrecteException;
 import org.formacio.setmana2.repositori.RepositoriEscola;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
