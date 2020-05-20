6c8ab01038af8915c27be1ff502fa768a59580d8
Javi <javibarrientosfernandez99@gmail.com>
2020-05-20 21:38:11 +0200

    feat: Añadir logica apunta

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -1,19 +1,39 @@
 package org.formacio.setmana2.servei;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.formacio.setmana2.domini.Matricula;
 import org.formacio.setmana2.repositori.EdatIncorrecteException;
+import org.formacio.setmana2.repositori.RepositoriEscola;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional(rollbackOn={EdatIncorrecteException.class})
 public class ServeiEscola {
-
+	
+	@Autowired
+	private RepositoriEscola escuela;
 	
 	/**
 	 * Important: els alumnes i els cursos indicats JA existeixen a la base de dades.
 	 * Per tant, els hem de carregar, no crear de nou.
 	 * L'excepcio EdatIncorrecteException no s'ha de capturar. S'ha de propagar cap el client
 	 */
-	public List<Matricula> apunta (String curs, List<String> alumnes) throws EdatIncorrecteException {
-		return null;
-    }
+	
+	public List<Matricula> apunta(String curs, List<String> alumnes) throws EdatIncorrecteException {
+		List<Matricula> listaDeMatriculas = new ArrayList<Matricula>();
+		
+		for (String alumno : alumnes) {
+			listaDeMatriculas.add(escuela.apunta(alumno, curs));
+		}
+		
+		return listaDeMatriculas;
+	}
 }
