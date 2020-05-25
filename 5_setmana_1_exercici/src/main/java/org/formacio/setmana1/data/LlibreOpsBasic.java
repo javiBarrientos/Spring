43d44298587f11c34ec2a586c221f66d65867e58
Javi <javibarrientosfernandez99@gmail.com>
2020-05-25 11:18:27 +0200

    refactor: Añadir logica dar de alta libro

--- 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -3,9 +3,11 @@ package org.formacio.setmana1.data;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -22,8 +24,9 @@ public class LlibreOpsBasic {
 	/**
 	 * Retorna el llibre amb l'ISBN indicat o, si no existeix, llança un LlibreNoExisteixException
 	 */
+	
 	public Llibre carrega(String isbn) throws LlibreNoExisteixException {
-		if (isbn == null) {
+		if (isbn != null) {
 			return em.find(Llibre.class, isbn);
 		} else {
 			throw new LlibreNoExisteixException();
@@ -33,7 +36,17 @@ public class LlibreOpsBasic {
 	/**
 	 * Sense sorpreses: dona d'alta un nou llibre amb les propietats especificaques
 	 */
-	public void alta (String isbn, String autor, Integer pagines, Recomanacio recomanacio, String titol) {
+	@Transactional
+	public void alta(String isbn, String autor, Integer pagines, Recomanacio recomanacio, String titol) {
+		Llibre libro = new Llibre();
+		
+		libro.setIsbn(isbn);
+		libro.setAutor(autor);
+		libro.setPagines(pagines);
+		libro.setRecomanacio(recomanacio);
+		libro.setTitol(titol);
+		
+		em.persist(libro);
 	}
 	
 	/**
@@ -41,21 +54,21 @@ public class LlibreOpsBasic {
 	 * @param isbn del llibre a eliminar
 	 * @return true si s'ha esborrat el llibre, false si no existia
 	 */
-	public boolean elimina (String isbn) {
+	public boolean elimina(String isbn) {
 		return true;
 	}
 	
 	/**
 	 * Guarda a bbdd l'estat del llibre indicat
 	 */
-	public void modifica (Llibre llibre) {
+	public void modifica(Llibre llibre) {
 	}
 	
 	/**
 	 * Retorna true o false en funcio de si existeix un llibre amb aquest ISBN
 	 * (Aquest metode no llanca excepcions!)
 	 */
-	public boolean existeix (String isbn) {
+	public boolean existeix(String isbn) {
 		return false;
 	}
 
@@ -63,7 +76,7 @@ public class LlibreOpsBasic {
 	 * Retorna quina es la recomanacio per el llibre indicat
 	 * Si el llibre indicat no existeix, retorna null
 	 */
-	public Recomanacio recomenacioPer (String isbn) {
+	public Recomanacio recomenacioPer(String isbn) {
 		return null;
 	}
 	
